348ee35f48a12e7b02597a98f792d4e159348ae8
Ekaterina Terentyeva <101096373+EkaterinaTerentyeva@users.noreply.github.com>
2023-06-11 21:32:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,45 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 import plotly_express as px
 
-df = pd.read_csv('vehicles_us.csv')
-#read the data
-df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
-df['is_4wd'] = df['is_4wd'].where(df['is_4wd'] != 1.0 , 'yes')
-df['is_4wd'] = df['is_4wd'].fillna('no')
-df['model_year'] = df['model_year'].fillna(df.groupby('model')
-['model_year'].transform('median'))
-df['cylinders'] = df['cylinders'].fillna(df.groupby('model')
-['cylinders'].transform('median'))
-df['odometer'] = df['odometer'].fillna(df.groupby('model_year')
-['odometer'].transform('median'))
-df['paint_color'] = df['paint_color'].fillna('No info')
-#replace four wheel drive values with yes and no
-st.title('Figures on car sales')
-#A scatterplot should be able to show a strong or week correlation between two variables
-#The two variables ill select is price and odometer
-#I was able to observe that odometer contains some missing information so i will remove the cars with missing values
-#I will be able to observe the strength of the correlation between these variables
-#i will now create a data set with all missing values in the odometer collumn removed
+df_car_ads = pd.read_csv('vehicles_us.csv')
 
-df_model_year = df.dropna(subset = ['odometer'])
-st.header('Scatter Plot: Price VS Odometer (make a selection to see cars by condition)')
-cond_list = sorted(df['condition'].unique())
-condition = st.selectbox(
-    label= 'select condition',
-    options= cond_list,
-    index= cond_list.index('new')
-)
-df_conditon = df[df['condition'] == condition]
+df_car_ads_mod = pd.DataFrame(df_car_ads.loc[df_car_ads["model_year"] > 2000])
+df_car_ads_mod= pd.DataFrame(df_car_ads_mod.loc[df_car_ads_mod['price'] < 42498])
 
-fig = px.scatter(df_conditon , 
-                 x = 'odometer', 
-                 y = 'price', 
-                 color = 'condition',
-                 title = 'Odometer vs Price (Make selection to see different car condition selections)',
-                 hover_name = 'model'
-                 )
-#also contains name of car
-st.write(fig)
+st.header('The Number of Days Ad is Listed Per Price and Vehicle Condition')
+fig = px.scatter(df_car_ads_mod, x="days_listed", y="price", color="condition",
+                 size='days_listed', hover_data=['make','type'], color_discrete_sequence=px.colors.qualitative.Light24,
+                 width = 1000, height = 1000, title='The Number of Days Ad is Listed Per Price and Condition of Vehicle')
+fig.show()
 
-st.header('Histogram: Manfacturer and price readings')
 
-manufac_list = sorted(df['manufacturer'].unique())
-manufacturer_1 = st.selectbox('Select manufacturer 1',
-                              manufac_list, index=manufac_list.index('chevrolet'))
+ads_by_make= pd.DataFrame(df_car_ads_mod.groupby('make')['model'].count())
+ads_by_make.reset_index(inplace=True)
+ads_by_make.columns = ['make', 'number of ads']
 
-manufacturer_2 = st.selectbox('Select manufacturer 2',
-                              manufac_list, index=manufac_list.index('hyundai'))
-mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
-df_filtered = df[mask_filter]
-normalize = st.checkbox('Normalize histogram', value=True)
-if normalize:
-    histnorm = 'percent'
-else:
-    histnorm = None
-st.write(px.histogram(df_filtered,
-                      x='price',
-                      nbins=30,
-                      color='manufacturer',
-                      histnorm=histnorm,
-                      barmode='overlay'))
+st.header('Number of Ads Per Car Manufacturer')
+fig = px.pie(ads_by_make,
+             values='number of ads',
+             names='make',
+             title='Number of Ads Per Car Manufacturer',
+             color='make',
+             color_discrete_sequence=px.colors.qualitative.Light24)
+
+fig.show()
+
+
+ads_by_type= pd.DataFrame(df_car_ads_mod.groupby('type')['make'].count())
+ads_by_type.reset_index(inplace=True)
+ads_by_type.columns = ['type', 'number_of_ads']
+
+st.header('Number of Ads Per Vehicle Type')
+fig = px.bar(ads_by_type,
+             x = 'type',
+             y = 'number_of_ads',
+             color='type',
+             color_discrete_sequence=px.colors.qualitative.Light24,
+            title='Number of Ads Per Vehicle Type')
+fig.show()
+
 
